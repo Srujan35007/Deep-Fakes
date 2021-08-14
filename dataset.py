5bb35f106c7a667b683d2ac59e72bf55bc72e4ac
Srujan35007 <srujan35007@gmail.com>
2021-08-14 17:37:34 +0530

    models,dataset done

--- dataset.py
+++ dataset.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np 
 import cv2 
 import os 
 
@@ -18,8 +17,7 @@ class FaceData(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         img = cv2.imread(self.file_paths[index])
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = np.asarray(cv2.resize(img, (128,128)))/255. + 0.0001
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRGB)
         if self.transform:
             img = self.transform(img)
-        return img.float().view(1,128,128) 
+        return img.float() 
--- models.py
+++ models.py
@@ -1,51 +1,97 @@
 import torch
 import torch.nn as nn 
-#from torchsummary import summary
+from torchsummary import summary
 
-class AutoEncoderConv(nn.Module):
-    def __init__(self, input_channels):
-        super(AutoEncoderConv, self).__init__()
-        self.model_name = 'AutoEncoderConv_Face'
-        self.Encoder = nn.Sequential(
-        self.ConvBlock(input_channels,64,3,1,1),
+class Encoder(nn.Module):
+    def __init__(self, img_channels):
+        self.model_name = 'Face_Encoder_Conv'
+        super(Encoder, self).__init__()
+        self.layers = nn.Sequential(
+        # in: image_channels x dim x dim
+        self.encoder_block(img_channels,16,3,1,1),
         nn.AvgPool2d(2),
-        self.ConvBlock(64,128,3,1,1),
+        # out: 16 x dim/2 x dim/2
+        self.encoder_block(16,32,3,1,1),
         nn.AvgPool2d(2),
-        self.ConvBlock(128,256,3,1,1)
+        # out: 32 x dim/4 x dim/4
+        self.encoder_block(32,64,3,1,1),
+        nn.AvgPool2d(2),
+        # out: 64 x dim/8 x dim/8
+        self.encoder_block(64,128,3,1,1),
+        nn.AvgPool2d(2),
+        # out: 128 x dim/16 x dim/16
+        self.encoder_block(128,256,3,1,1),
+        # out: 256 x dim/16 x dim/16
         )
-        self.Decoder = nn.Sequential(
-        self.ConvBlock(256,128,3,1,1),
-        nn.UpsamplingBilinear2d(scale_factor=2),
-        self.ConvBlock(128,64,3,1,1),
-        nn.UpsamplingBilinear2d(scale_factor=2),
-        self.ConvBlock(64,input_channels,3,1,1),
-        )
-        self.init_weights()
+        self.init_weigths()
 
-    def ConvBlock(self, ins_, outs_, kernel_, stride_, padding_):
+    def encoder_block(self, ins_,outs_,kernel_,stride_,padding_):
         return nn.Sequential(
-        nn.Conv2d(ins_, ins_*2, kernel_, stride_, padding_, bias=False),
+        nn.Conv2d(ins_,ins_*2,kernel_,stride_,padding_,bias=False),
         nn.BatchNorm2d(ins_*2),
         nn.ReLU(inplace=True),
-        nn.Conv2d(ins_*2, outs_, kernel_, stride_, padding_, bias=False),
+        nn.Conv2d(ins_*2,outs_,kernel_,stride_,padding_,bias=False),
         nn.BatchNorm2d(outs_),
-        nn.ReLU(inplace=True)
+        nn.ReLU(inplace=True),
         )
+    
+    def forward(self, x):
+        return self.layers(x)
 
-    def forward(self,x):
-        return self.Decoder(self.Encoder(x))
-
-    def init_weights(self):
-        _count_module, _total_module = 0, 0
+    def init_weigths(self):
+        count_, total_ = 0, 0
         for module in self.parameters():
-            if len(module.shape) >= 2:
+            if len(module.shape) > 1:
                 nn.init.kaiming_normal_(module)
-                _count_module += 1
-                _total_module += 1
-            #elif len(module.shape) == 1:
-            #    nn.init.normal_(module, mean=0., std=0.414)
-            #    _count_module += 1
-            #    _total_module += 1
+                count_ += 1
+                total_ += 1
             else:
-                _total_module += 1
-        print(f"Model weights initialized ({_count_module}/{_total_module})")
+                total_ += 1
+        print(f'{self.model_name} weights initialized ({count_}/{total_})')
+
+class Decoder(nn.Module):
+    def __init__(self, in_channels, img_channels):
+        self.model_name = 'Face_Decoder_Conv'
+        super(Decoder, self).__init__()
+        self.layers = nn.Sequential(
+        # in: in_channels x dim x dim
+        self.decoder_block(in_channels,256,3,1,1),
+        nn.UpsamplingBilinear2d(scale_factor=2),
+        # out: 256 x dim*2 x dim*2
+        self.decoder_block(256,128,3,1,1),
+        nn.UpsamplingBilinear2d(scale_factor=2),
+        # out: 128 x dim*4 x dim*4
+        self.decoder_block(128,64,3,1,1),
+        nn.UpsamplingBilinear2d(scale_factor=2),
+        # out: 64 x dim*8 x dim*8
+        self.decoder_block(64,32,3,1,1),
+        nn.UpsamplingBilinear2d(scale_factor=2),
+        # out: 32 x dim*16 x dim*16
+        self.decoder_block(32,img_channels,3,1,1),
+        # out: img_channels x dim*16 x dim*16
+        )
+        self.init_weigths()
+
+    def decoder_block(self, ins_,outs_,kernel_,stride_,padding_):
+        return nn.Sequential(
+        nn.Conv2d(ins_,outs_,kernel_,stride_,padding_,bias=False),
+        nn.BatchNorm2d(outs_),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(outs_,outs_,kernel_,stride_,padding_,bias=False),
+        nn.BatchNorm2d(outs_),
+        nn.ReLU(inplace=True),
+        )
+    
+    def forward(self, x):
+        return self.layers(x)
+
+    def init_weigths(self):
+        count_, total_ = 0, 0
+        for module in self.parameters():
+            if len(module.shape) > 1:
+                nn.init.kaiming_normal_(module)
+                count_ += 1
+                total_ += 1
+            else:
+                total_ += 1
+        print(f'{self.model_name} weights initialized ({count_}/{total_})')
